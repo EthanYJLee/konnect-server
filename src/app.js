f83157162e687c7f1a04522ed9aaf92e23379196
EthanYJLee <lyj72011648@gmail.com>
2025-06-01 20:37:16 +0900

    cors 추가

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:5000",
       "http://lyj72011648.cafe24.com",
+      "http://konnect.guru",
+      "https://konnect.guru",
     ],
     // origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
